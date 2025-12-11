848976ebe0b2a48e55618678b9a9910a57b320fb
heewonn09 <heewonn09@naver.com>
2025-12-12 02:41:07 +0900

    입출차 조회 기능 추가

--- backend/src/main/java/com/iot2ndproject/mobilityhub/domain/work/repository/WorksearchRepository.java
+++ backend/src/main/java/com/iot2ndproject/mobilityhub/domain/work/repository/WorksearchRepository.java
@@ -1,4 +1,15 @@
 package com.iot2ndproject.mobilityhub.domain.work.repository;
 
-public interface WorksearchRepository {
+import com.iot2ndproject.mobilityhub.domain.work.entity.WorkInfoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface WorksearchRepository extends JpaRepository<WorkInfoEntity, Long> {
+
+    List<WorkInfoEntity> findByEntryTimeBetween(LocalDateTime start, LocalDateTime end);
+    List<WorkInfoEntity> findByExitTimeBetween(LocalDateTime start, LocalDateTime end);
+
 }
