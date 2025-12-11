f447e43ebc5000ca065e7c0d3670da7316db2ab6
heewonn09 <heewonn09@naver.com>
2025-12-11 23:37:42 +0900

    수정

--- backend/src/main/java/com/iot2ndproject/mobilityhub/domain/vehicle/repository/CarRepository.java
+++ backend/src/main/java/com/iot2ndproject/mobilityhub/domain/vehicle/repository/CarRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface CarRepository extends JpaRepository<CarEntity,Integer> {
-    Optional<CarEntity> findByCarNumber(String carNumber );
-
+public interface CarRepository extends JpaRepository<CarEntity,Long> {
+    Optional<CarEntity> findByCarNumber(String carNumber);
 }
