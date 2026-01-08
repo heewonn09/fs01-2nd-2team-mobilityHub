070228afa6a61d9ddfa0952f6eed511ab8a3584e
heewonn09 <heewonn09@naver.com>
2026-01-09 01:16:37 +0900

    불필요 코드 제거 및 주석처리 작업

--- frontend/admin/src/api/EntranceAPI.js
+++ frontend/admin/src/api/EntranceAPI.js
@@ -21,20 +21,6 @@ export const getTodayExit = async () => {
    OCR / 입차 관련
 ========================= */
 
-// 최근 인식 번호판
-export const getLatestEntrance = async () => {
-  const res = await backendServer.get("/entrance/latest");
-  return res.data;
-};
-
-// OCR 번호 수정 (image 기준)
-export const updateOcrNumber = async (imageId, carNumber) => {
-  return backendServer.put(`/entrance/image/${imageId}/ocr`, {
-    carNumber,
-  });
-};
-// OCR 번호 저장??
-
 // 입차 승인
 export const approveEntrance = async (workId) => {
   return backendServer.post(`/entrance/${workId}/approve`);
